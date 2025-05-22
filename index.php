8050adb641be226bb413d9fb40609d797021257c
mk1871 <manuelkas2022@gmail.com>
2025-05-22 00:13:25 -0500

    Modified database settings for Windows

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ require 'Database.php';
 // connect to our MariaDB database and execute a query
 $config = require 'config.php';
 
-$db    = new Database($config['database'], 'root', 'firstdb2025.');
+// variable para Linux
+//$db    = new Database($config['database'], 'root', 'firstdb2025.');
+
+// Variable para Windows
+$db    = new Database($config['database'], 'root', 'password');
 $posts = $db->query('select * from posts')->fetchAll();
 
 dd($posts);
